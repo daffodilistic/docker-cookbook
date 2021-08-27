ecfbf21e618fbb70bd369935838e313f9b63dcef
Soh Thiam Hing <daffodilistic@gmail.com>
2021-08-27 09:59:30 +0800

    Add script to alias docker-compose for Google Container-Optimized OS

---
+++ serve-gcos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo alias docker-compose="'"'docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker/compose'"'" >> ~/.bashrc
+source ~/.bashrc
+docker-compose -f docker-compose.yml up
